b6d9de6cff5c80877897cf678d926fedd96e29f0
Vinsee <anuvyas2405@gmail.com>
2025-03-17 02:34:13 +0530

    Created required components for SPA

---
+++ src/components/JobListings.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import Jobs from '../BrowseJobs.json'
+import JobList from './JobList';
+import { useState, useEffect } from 'react';
+
+const JobListings = ({isHome = true}) => {
+  const [jobs, setJobs] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchJobs = async () => {
+      const res = await fetch('http://localhost:8000')
+    }
+  });
+
+   return (
+    <section className="bg-blue-50 px-4 py-10">
+      <div className="container-xl lg:container m-auto">
+        <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
+          { isHome ? 'Recent Jobs' : 'Browse Jobs' }
+        </h2>
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+
+        {JobListings.map((job) => (
+            <JobList key ={job.id} job = {job}/>
+        ))}
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default JobListings
